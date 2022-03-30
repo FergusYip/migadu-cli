d55c033be3e4dd686ecf114f314c3b279d5e8cff
Fergus Yip <contact@fergusyip.me>
2022-03-30 17:31:31 +1100

    Add description

--- cmd/root.go
+++ cmd/root.go
@@ -10,18 +10,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "migadu-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A CLI for Migadu",
+	Long:  `This application is a tool to manage mailboxes, identities, aliases, and rewrites for your Migadu account.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -47,5 +40,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
